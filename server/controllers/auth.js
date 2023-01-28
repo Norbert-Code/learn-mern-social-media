c0fb06d36fd1d5eaeac4fdc714be9937aa783c22
Norbert Kiss <norbert.kiss@door2solution.com>
2023-01-28 15:47:42 +0100

    remove the password form the response

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -48,7 +48,10 @@ export const login = async (req, res) => {
     if (!isMatch) {
       return res.status(400).json({ msg: "Invalid credentials."});
     }
-    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET)
+    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+    //delete the userpassword to not send to frontend
+    delete user.password;
+    res.status(200).json({token,user});
   } catch (err) {
     res.status(500).json({ error: err.message});
   }

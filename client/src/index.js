2bc9c8519faae9c1d7be087d4d7f1bf527779396
Norbert Kiss <norbert.kiss@door2solution.com>
2023-02-04 10:35:14 +0100

    color, theme, darkmode, styling setup

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,8 @@ import authReducer from "./state";
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import {
-  persisStore,
-  persisReducer,
+  persistStore,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -15,10 +15,30 @@ import {
   PURGE,
   REGISTER
 } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+//import { getDefaultNormalizer } from '@testing-library/react';
+//still be store in the localstore 
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>      
+    </Provider>    
   </React.StrictMode>
 );

77bb50fa092fd1aad87112e2972dda34650f5491
Norbert Kiss <norbert.kiss@door2solution.com>
2023-02-04 10:19:11 +0100

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  persisStore,
+  persisReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
